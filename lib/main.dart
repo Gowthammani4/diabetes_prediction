e29f7f390cf1de03cb4bdd205409b56b11888481
Gowthammani4 <88430577+Gowthammani4@users.noreply.github.com>
2023-02-18 16:08:22 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,140 @@
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
-  // http://127.0.0.1:5000/predict?pregnancies=2&glucose=130&bloodpressure=89&skinthickness=20&insulin=90&bmi=25&dpf=0.4&age=50
+  runApp(MyApp());
+//https://diabetes-prediction-pmn1.onrender.com/predict?pregnancies=2&glucose=150&bloodpressure=89&skinthickness=20&insulin=90&bmi=25&dpf=0.4&age=50
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  final _formKey = GlobalKey<FormState>();
+
+  final myController = TextEditingController();
+  var result = "";
+  List<double> a=[];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Salary Prediction App'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        //a.add(myController.value)
+        body: Form(
+          key: _formKey,
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextFormField(
+                  controller: myController,
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.scatter_plot),
+                    hintText: 'pregnancies',
+                    labelText: 'Number of Pregnancies eg. 0',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.scatter_plot),
+                    hintText: 'glucose',
+                    labelText: 'Number of Pregnancies eg. 0',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.scatter_plot),
+                    hintText: 'bloodpressure',
+                    labelText: 'Blood Pressure (mmHg) eg. 80',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.scatter_plot),
+                    hintText: 'skinthickness',
+                    labelText: 'Skin Thickness (mm) eg. 20',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.scatter_plot),
+                    hintText: 'insulin',
+                    labelText: 'Insulin Level (IU/mL) eg. 80',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.scatter_plot),
+                    hintText: 'bmi',
+                    labelText: 'Body Mass Index (kg/m²) eg. 23.1',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.scatter_plot),
+                    hintText: 'dpf',
+                    labelText: 'Diabetes Pedigree Function eg. 0.52',
+                  ),
+                ),
+                TextFormField(
+                  controller: myController,
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.scatter_plot),
+                    hintText: 'age',
+                    labelText: 'Age (years) eg. 34',
+                  ),
+                ),
+                Container(
+                    padding: const EdgeInsets.only(left: 150.0, top: 20.0),
+                    child: ElevatedButton(
+                      // ignore: prefer_const_constructors
+                      child: Text('Predict',
+                          style: TextStyle(fontSize: 20, color: Colors.white)),
+                      onPressed: predict,
+                    )),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Center(
+                      child: Text(
+                    result,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )),
+                )
+              ]),
+        ));
+  }
+
+  predict() async {
+    setState(() {
+      this.result = 'Predicting...Please wait for the result';
+    });
+
+    String url =
+        'https://diabetes-prediction-pmn1.onrender.com/predict?pregnancies=2&glucose=150&bloodpressure=89&skinthickness=20&insulin=90&bmi=25&dpf=0.4&age=50';
+    var j = await http.get(Uri.parse(url));
+    var json = j.body;
+    print(json);
+    var data = jsonDecode(json);
+    print(data);
+    var result = data[0];
+    print(result);
+    var salary = result.toStringAsFixed(2);
+    print(salary);
+    setState(() {
+      this.result =
+          'Estimate Salary should be around - Rs.' + salary.toString();
+    });
   }
 }
