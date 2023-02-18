d2be0d943a7915ed8a0147b0b1c82a8b82f65634
Gowthammani4 <gowthammani7065@gmail.com>
2023-02-18 15:27:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+# Importing essential libraries
+from flask import Flask, render_template,request
+
+import pickle
+import numpy as np
+from numpy.core.multiarray import array
+# import requests
+
+# Load the Random Forest CLassifier model
+filename = 'diabetes-prediction-rfc-model.pkl'
+classifier = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+@app.route('/predict', )
+def predict():
+        a=request.args.to_dict()
+        b=[]
+        for key,value in a.items():
+            b.append(float(value))
+        my_prediction = classifier.predict([b])
+        if my_prediction[0]==0:
+            return str("No diabetes")
+        else:
+            return str("Diabetes")
+if __name__ == '__main__':
+	app.run(debug=True)
