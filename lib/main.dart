8cefe5d60f97d86aeec00686d5d637b107df5d1d
Gowthammani4 <gowthammani7065@gmail.com>
2023-02-18 20:10:53 +0530

    currently,its workiing.Wants to modify somethings

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
-//https://diabetes-prediction-pmn1.onrender.com/predict?pregnancies=2&glucose=150&bloodpressure=89&skinthickness=20&insulin=90&bmi=25&dpf=0.4&age=50
 }
 
 class MyApp extends StatefulWidget {
@@ -38,7 +37,6 @@ class _MyAppState extends State<MyApp> {
             appBar: AppBar(
               title: const Text('Diabetes prediction App'),
             ),
-            //a.add(myController.value)
             body: SingleChildScrollView(
               child: Form(
                 key: _formKey,
@@ -75,7 +73,7 @@ class _MyAppState extends State<MyApp> {
                       TextFormField(
                         controller: myController,
                         decoration: const InputDecoration(
-                          icon: const Icon(Icons.scatter_plot),
+                          icon: Icon(Icons.scatter_plot),
                           hintText: 'bloodpressure',
                           labelText: 'Blood Pressure (mmHg) eg. 80',
                         ),
@@ -86,7 +84,7 @@ class _MyAppState extends State<MyApp> {
                       TextFormField(
                         controller: myController,
                         decoration: const InputDecoration(
-                          icon: const Icon(Icons.scatter_plot),
+                          icon: Icon(Icons.scatter_plot),
                           hintText: 'skinthickness',
                           labelText: 'Skin Thickness (mm) eg. 20',
                         ),
@@ -99,7 +97,7 @@ class _MyAppState extends State<MyApp> {
                       TextFormField(
                         controller: myController,
                         decoration: const InputDecoration(
-                          icon: const Icon(Icons.scatter_plot),
+                          icon: Icon(Icons.scatter_plot),
                           hintText: 'insulin',
                           labelText: 'Insulin Level (IU/mL) eg. 80',
                         ),
@@ -113,7 +111,7 @@ class _MyAppState extends State<MyApp> {
                       TextFormField(
                         controller: myController,
                         decoration: const InputDecoration(
-                          icon: const Icon(Icons.scatter_plot),
+                          icon: Icon(Icons.scatter_plot),
                           hintText: 'bmi',
                           labelText: 'Body Mass Index (kg/m²) eg. 23.1',
                         ),
@@ -156,7 +154,7 @@ class _MyAppState extends State<MyApp> {
                           child: ElevatedButton(
                               // ignore: prefer_const_constructors
                               child: Text('Predict',
-                                  style: TextStyle(
+                                  style: const TextStyle(
                                       fontSize: 20, color: Colors.white)),
                               onPressed: predict)),
                       Padding(
@@ -164,7 +162,7 @@ class _MyAppState extends State<MyApp> {
                         child: Center(
                             child: Text(
                           result,
-                          style: TextStyle(fontWeight: FontWeight.bold),
+                          style: const TextStyle(fontWeight: FontWeight.bold),
                         )),
                       )
                     ]),
@@ -176,21 +174,10 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       this.result = 'Predicting...Please wait for the result';
     });
-    print('*********************');
-    print(a);
-    print("bmi" + a['bmi']!);
-
     String url =
         'https://diabetes-prediction-pmn1.onrender.com/predict?pregnancies=${a['pregnancies']}&glucose=${a['glucose']}&bloodpressure=${a['bloodpressure']}&skinthickness=${a['skinthickness']}&insulin=${a['insulin']}&bmi=${a['bmi']}&dpf=${a['dpf']}&age=${a['age']}';
     var j = await http.get(Uri.parse(url));
     var json = j.body;
-    print(json);
-    // var data = jsonDecode(json);
-    // print(data);
-    // var resul = data[0];
-    // print(resul);
-    // var salary = resul;
-    // print(salary);
     setState(() {
       result = json;
     });
