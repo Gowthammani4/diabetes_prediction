ae51c4f3f9f0c8e6ac2bfd83396083185632d543
Gowthammani4 <gowthammani7065@gmail.com>
2023-02-18 20:08:29 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -20,146 +20,179 @@ class _MyAppState extends State<MyApp> {
 
   final myController = TextEditingController();
   var result = "";
-  List<String> a = [];
+  Map<String, String> a = {
+    'pregnancies': '',
+    'glucose': '',
+    'bloodpressure': '',
+    'skinthickness': '',
+    'insulin': '',
+    'bmi': '',
+    'dpf': '',
+    'age': ''
+  };
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Salary Prediction App'),
-        ),
-        //a.add(myController.value)
-        body: Form(
-          key: _formKey,
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextFormField(
-                  controller: myController,
-                  decoration: const InputDecoration(
-                    icon: const Icon(Icons.scatter_plot),
-                    hintText: 'pregnancies',
-                    labelText: 'Number of Pregnancies eg. 0',
-                  ),
-                  validator: (value) {
-                    a.add(value!);
-                  },
-                ),
-                TextFormField(
-                  controller: myController,
-                  decoration: const InputDecoration(
-                    icon: const Icon(Icons.scatter_plot),
-                    hintText: 'glucose',
-                    labelText: 'Number of Pregnancies eg. 0',
-                  ),
-                  validator: (value) {
-                    a.add(value!);
-                  },
-                ),
-                TextFormField(
-                  controller: myController,
-                  decoration: const InputDecoration(
-                    icon: const Icon(Icons.scatter_plot),
-                    hintText: 'bloodpressure',
-                    labelText: 'Blood Pressure (mmHg) eg. 80',
-                  ),
-                  validator: (value) {
-                    a.add(value!);
-                  },
-                ),
-                TextFormField(
-                  controller: myController,
-                  decoration: const InputDecoration(
-                    icon: const Icon(Icons.scatter_plot),
-                    hintText: 'skinthickness',
-                    labelText: 'Skin Thickness (mm) eg. 20',
-                  ),
-                  validator: (value) {
-                    a.add(value!);
-                  },
-                ),
-                TextFormField(
-                  controller: myController,
-                  decoration: const InputDecoration(
-                    icon: const Icon(Icons.scatter_plot),
-                    hintText: 'insulin',
-                    labelText: 'Insulin Level (IU/mL) eg. 80',
-                  ),
-                  validator: (value) {
-                    a.add(value!);
-                  },
-                ),
-                TextFormField(
-                  controller: myController,
-                  decoration: const InputDecoration(
-                    icon: const Icon(Icons.scatter_plot),
-                    hintText: 'bmi',
-                    labelText: 'Body Mass Index (kg/m²) eg. 23.1',
-                  ),
-                  validator: (value) {
-                    a.add(value!);
-                  },
-                ),
-                TextFormField(
-                  controller: myController,
-                  decoration: const InputDecoration(
-                    icon: const Icon(Icons.scatter_plot),
-                    hintText: 'dpf',
-                    labelText: 'Diabetes Pedigree Function eg. 0.52',
-                  ),
-                  validator: (value) {
-                    a.add(value!);
-                  },
-                ),
-                TextFormField(
-                  controller: myController,
-                  decoration: const InputDecoration(
-                    icon: const Icon(Icons.scatter_plot),
-                    hintText: 'age',
-                    labelText: 'Age (years) eg. 34',
-                  ),
-                  validator: (value) {
-                    a.add(value!);
-                  },
-                ),
-                Container(
-                    padding: const EdgeInsets.only(left: 150.0, top: 20.0),
-                    child: ElevatedButton(
-                      // ignore: prefer_const_constructors
-                      child: Text('Predict',
-                          style: TextStyle(fontSize: 20, color: Colors.white)),
-                      onPressed: predict,
-                    )),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Center(
-                      child: Text(
-                    result,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )),
-                )
-              ]),
-        ));
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Diabetes prediction App'),
+            ),
+            //a.add(myController.value)
+            body: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      TextFormField(
+                        controller: myController,
+                        decoration: const InputDecoration(
+                          icon: const Icon(Icons.scatter_plot),
+                          hintText: 'pregnancies',
+                          labelText: 'Number of Pregnancies eg. 0',
+                        ),
+                        onFieldSubmitted: (value) {
+                          setState(() {
+                            a['pregnancies'] = value;
+                          });
+                          // a.add(value!);
+                        },
+                      ),
+                      TextFormField(
+                        controller: myController,
+                        decoration: const InputDecoration(
+                          icon: const Icon(Icons.scatter_plot),
+                          hintText: 'glucose',
+                          labelText: 'Glucose level',
+                        ),
+                        onFieldSubmitted: (value) {
+                          setState(() {
+                            a['glucose'] = value;
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        controller: myController,
+                        decoration: const InputDecoration(
+                          icon: const Icon(Icons.scatter_plot),
+                          hintText: 'bloodpressure',
+                          labelText: 'Blood Pressure (mmHg) eg. 80',
+                        ),
+                        onFieldSubmitted: (value) {
+                          a['bloodpressure'] = value;
+                        },
+                      ),
+                      TextFormField(
+                        controller: myController,
+                        decoration: const InputDecoration(
+                          icon: const Icon(Icons.scatter_plot),
+                          hintText: 'skinthickness',
+                          labelText: 'Skin Thickness (mm) eg. 20',
+                        ),
+                        onFieldSubmitted: (value) {
+                          setState(() {
+                            a['skinthickness'] = value;
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        controller: myController,
+                        decoration: const InputDecoration(
+                          icon: const Icon(Icons.scatter_plot),
+                          hintText: 'insulin',
+                          labelText: 'Insulin Level (IU/mL) eg. 80',
+                        ),
+                        onFieldSubmitted: (value) {
+                          setState(() {
+                            a['insulin'] = value;
+                          });
+                          // a.add(value!);
+                        },
+                      ),
+                      TextFormField(
+                        controller: myController,
+                        decoration: const InputDecoration(
+                          icon: const Icon(Icons.scatter_plot),
+                          hintText: 'bmi',
+                          labelText: 'Body Mass Index (kg/m²) eg. 23.1',
+                        ),
+                        onFieldSubmitted: (value) {
+                          setState(() {
+                            a['bmi'] = value;
+                          });
+                        },
+                      ),
+                      TextFormField(
+                        controller: myController,
+                        decoration: const InputDecoration(
+                          icon: const Icon(Icons.scatter_plot),
+                          hintText: 'dpf',
+                          labelText: 'Diabetes Pedigree Function eg. 0.52',
+                        ),
+                        onFieldSubmitted: (value) {
+                          setState(() {
+                            a['dpf'] = value;
+                          });
+                          //setState(() {});
+                        },
+                      ),
+                      TextFormField(
+                        controller: myController,
+                        decoration: const InputDecoration(
+                          icon: const Icon(Icons.scatter_plot),
+                          hintText: 'age',
+                          labelText: 'Age (years) eg. 34',
+                        ),
+                        onFieldSubmitted: (value) {
+                          setState(() {
+                            a['age'] = value;
+                          });
+                        },
+                      ),
+                      Container(
+                          padding:
+                              const EdgeInsets.only(left: 150.0, top: 20.0),
+                          child: ElevatedButton(
+                              // ignore: prefer_const_constructors
+                              child: Text('Predict',
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white)),
+                              onPressed: predict)),
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Center(
+                            child: Text(
+                          result,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )),
+                      )
+                    ]),
+              ),
+            )));
   }
 
   predict() async {
     setState(() {
       this.result = 'Predicting...Please wait for the result';
     });
+    print('*********************');
+    print(a);
+    print("bmi" + a['bmi']!);
 
     String url =
-        'https://diabetes-prediction-pmn1.onrender.com/predict?pregnancies=${a[0]}&glucose=${a[1]}&bloodpressure=${a[2]}&skinthickness=${a[3]}&insulin=${a[4]}&bmi=${a[5]}&dpf=${a[6]}&age=${a[7]}';
+        'https://diabetes-prediction-pmn1.onrender.com/predict?pregnancies=${a['pregnancies']}&glucose=${a['glucose']}&bloodpressure=${a['bloodpressure']}&skinthickness=${a['skinthickness']}&insulin=${a['insulin']}&bmi=${a['bmi']}&dpf=${a['dpf']}&age=${a['age']}';
     var j = await http.get(Uri.parse(url));
     var json = j.body;
     print(json);
-    var data = jsonDecode(json);
-    print(data);
-    var result = data[0];
-    print(result);
-    var salary = result.toStringAsFixed(2);
-    print(salary);
+    // var data = jsonDecode(json);
+    // print(data);
+    // var resul = data[0];
+    // print(resul);
+    // var salary = resul;
+    // print(salary);
     setState(() {
-      this.result =
-          'Estimate Salary should be around - Rs.' + salary.toString();
+      result = json;
     });
   }
 }
